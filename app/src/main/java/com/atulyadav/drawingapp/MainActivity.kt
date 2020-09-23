a62bacace31095b80e07ce488d4d075c3c469627
Ashu1533 <ashuy033@gmail.com>
2020-09-23 22:28:14 +0530

    Added functionality to handle different Phone Sizes.

--- app/src/main/java/com/atulyadav/drawingapp/MainActivity.kt
+++ app/src/main/java/com/atulyadav/drawingapp/MainActivity.kt
@@ -2,10 +2,14 @@ package com.atulyadav.drawingapp
 
 import android.os.Bundle
 import androidx.appcompat.app.AppCompatActivity
+import kotlinx.android.synthetic.main.activity_main.*
 
 class MainActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
+
+        drawing_view.setSizeForBrush(20.toFloat())
+
     }
 }
